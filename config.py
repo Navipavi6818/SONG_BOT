d9dab6f3f40b56942ac81737d2e28934b9405900
Navipavi6818 <87847014+Navipavi6818@users.noreply.github.com>
2021-11-23 15:38:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 from youtube_dl import YoutubeDL
 
 class Config:
-API_ID = int(os.environ.get("API_ID", 12345))
-API_HASH = os.environ.get("API_HASH", None)
+APP_ID = int(os.environ.get("APP_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 PICS = environ.get("PICS", "")
