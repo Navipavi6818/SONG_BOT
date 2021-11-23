425bb2ea767de4540fe75aa1d441e0e768d86c92
Navipavi6818 <87847014+Navipavi6818@users.noreply.github.com>
2021-11-23 15:40:37 +0530

    Update Misc.py

--- LUNA_SONGBOT/Misc.py
+++ LUNA_SONGBOT/Misc.py
@@ -3,7 +3,6 @@
 import os
 from pyrogram import Client, filters
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-from info import IMDB_TEMPLATE
 from utils import extract_user, get_file_id, get_poster, last_online
 import time
 from datetime import datetime
