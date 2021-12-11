861b72ace3ab4d41a413710a91bf084fba680f9c
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-11 10:15:26 +0530

    Update music.py

--- LUNA_SONGBOT/modules/music.py
+++ LUNA_SONGBOT/modules/music.py
@@ -14,7 +14,7 @@ from yt_dlp import YoutubeDL
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
 from LUNA_SONGBOT import get_text, progress
-from LUNA_SONGBOT import app, arq
+from LUNA_SONGBOT import pbot, arq
 
 async def lyrics_func(answers, text):
     song = await arq.lyrics(text)
