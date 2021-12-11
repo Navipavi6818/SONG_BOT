5f97585dcfe3bac894063b8e786b40b2ac4def3a
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-11 10:19:26 +0530

    Create Script.py

---
+++ LUNA_SONGBOT/Script.py
@@ -0,0 +1,28 @@
+class script(object):
+    START_TXT = """𝙷𝙴𝙻𝙾 {},
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+    HELP_TXT = """𝙷𝙴𝚈 {}
+𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
+    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/N_A_V_I_P_A_V_I>MR PROFESSOR</a>
+✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
+✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄"""
+    SOURCE_TXT = """<b>NOTE:</b>
+➩ LUNA IS A OPEN SOURCE PROJECT  
+  <a href=https://github.com/Navipavi6818/LUNA_SONG_BOT>LUNASONGBOT</a>
+<b>DEVS:</b>
+➩ <a href=https://t.me/N_A_V_I_P_A_V_I>MR PROFESSOR</a>
+➩ <a href=https://t.me/shamilhabeeb>𝕊𝕞𝕃</a>
+➩ <a  href=https://github.com/M-fazin>M-fazin</a>
+    MUSIC = """Help: <b>🎧MUSIC🎧</b>
+
+➩ /music <songname artist(optional)>: uploads the song in it's best quality available"""
+ 
+    VSONG = """Help: <b>📀VSONG📀</b>
+
+➩ /vsong <songname artist(optional)>: uploads the video song in it's best quality available"""
+  
+    LYRICS = """Help: <b>🎶LYRICS🎶</b>
+
+➩ /lyrics <songname>: uploads the lyrics of song
